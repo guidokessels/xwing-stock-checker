059026cd02909ccf8e06ca8b8b6c7745f754cf50
guidokessels <guidokessels@users.noreply.github.com>
2020-02-24 04:17:05 +0000

    deploy: 53ff02196c4d19b596a96e5827a1e981491dcf9e

--- precache-manifest.737d19af5b67209f48af673b371bb727.js
+++ precache-manifest.737d19af5b67209f48af673b371bb727.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0670d0e829559525f97816886b15ce31",
+    "revision": "1cec55e1bd5a23e501036b3e17566fad",
     "url": "/xwing-stock-checker/index.html"
   },
   {
-    "revision": "977f787afe28bf932979",
+    "revision": "dc1c03212ee4428062b3",
     "url": "/xwing-stock-checker/static/css/main.e282cce5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/xwing-stock-checker/static/js/2.43bbc055.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "977f787afe28bf932979",
-    "url": "/xwing-stock-checker/static/js/main.33298be2.chunk.js"
+    "revision": "dc1c03212ee4428062b3",
+    "url": "/xwing-stock-checker/static/js/main.01b4dc12.chunk.js"
   },
   {
     "revision": "af01f38608f393ecff4e",
