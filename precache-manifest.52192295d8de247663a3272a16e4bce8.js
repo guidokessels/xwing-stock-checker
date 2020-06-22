a6cdbe5121a9db7dbae24f5dc05b37e90342c391
guidokessels <guidokessels@users.noreply.github.com>
2020-06-22 11:09:20 +0000

    deploy: 4a542c23847c59366453c35427cba0fc4f14bbb1

--- precache-manifest.52192295d8de247663a3272a16e4bce8.js
+++ precache-manifest.52192295d8de247663a3272a16e4bce8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6bc1ad7ba817804b11b58e9c42ba9819",
+    "revision": "b16cb76054c990ec9d61efe0adca000f",
     "url": "/xwing-stock-checker/index.html"
   },
   {
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/xwing-stock-checker/static/js/3.24f2f8b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "00060197c19b47384190",
-    "url": "/xwing-stock-checker/static/js/data.4c3f3679.chunk.js"
+    "revision": "0ed73e25bbe49177167f",
+    "url": "/xwing-stock-checker/static/js/data.efb5c93a.chunk.js"
   },
   {
     "revision": "a182b08c3f0106e75e1a",
     "url": "/xwing-stock-checker/static/js/main.61c9576f.chunk.js"
   },
   {
-    "revision": "d3db1c6ddc37fbd23d50",
-    "url": "/xwing-stock-checker/static/js/runtime-main.e9bb6ff5.js"
+    "revision": "0437f4dac357bcc8ccaa",
+    "url": "/xwing-stock-checker/static/js/runtime-main.37174c31.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/xwing-stock-checker/precache-manifest.dd0003edd977f1761485ece1f21b3f74.js"
+  "/xwing-stock-checker/precache-manifest.52192295d8de247663a3272a16e4bce8.js"
 );
 
 self.addEventListener('message', (event) => {
