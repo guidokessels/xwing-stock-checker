cdabe5bf60111f98a022f0076c2a5f9ec15478b0
guidokessels <guidokessels@users.noreply.github.com>
2022-11-04 15:51:53 +0000

    deploy: f1eb38c5a200ae920176e104cf3b7333909aa022

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/xwing-stock-checker/precache-manifest.d87a225447581be3337bddc6b1fcada4.js"
+  "/xwing-stock-checker/precache-manifest.83766f0fe3701810ac11609604700a3a.js"
 );
 
 self.addEventListener('message', (event) => {
